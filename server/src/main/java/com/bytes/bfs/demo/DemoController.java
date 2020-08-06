0cdc0061903d39e3f9f3562c94c486943b709e9c
麻亮 <maliang@newhope.cn>
2020-08-06 10:08:38 +0800

    feat(*) : 修改项目打包问题

---
+++ server/src/main/java/com/bytes/bfs/demo/DemoController.java
@@ -0,0 +1,17 @@
+package com.bytes.bfs.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@RestController
+@RequestMapping("/v1")
+public class DemoController {
+
+    @RequestMapping("/date")
+    public String getDate( ){
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss:sss"));
+    }
+}
