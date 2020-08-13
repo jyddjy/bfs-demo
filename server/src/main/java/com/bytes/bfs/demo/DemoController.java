b46bfa30f0a4b1f313696b3d106736dae0dfef0f
麻亮 <maliang@newhope.cn>
2020-08-13 19:08:54 +0800

    feat(*) : 测试接口

--- server/src/main/java/com/bytes/bfs/demo/DemoController.java
+++ server/src/main/java/com/bytes/bfs/demo/DemoController.java
@@ -7,11 +7,16 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 @RestController
-@RequestMapping("/v1")
+@RequestMapping("/demo")
 public class DemoController {
 
-    @RequestMapping("/date")
+    @RequestMapping("/test")
     public String getDate( ){
         return LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    }
+
+    @RequestMapping("/test2")
+    public String getDate2( ){
+        return "hello";
     }
 }
