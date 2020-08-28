188703fbea7b3de5d9e2f04ae55af2c78ef1f81a
麻亮 <maliang@newhope.cn>
2020-08-28 09:39:24 +0800

    feat(*) : 测试网关使用

--- server/src/main/java/com/bytes/bfs/demo/DemoController.java
+++ server/src/main/java/com/bytes/bfs/demo/DemoController.java
@@ -1,44 +1,43 @@
 package com.bytes.bfs.demo;
 
 import com.alibaba.fastjson.JSONObject;
-import com.bytes.bfs.demo.client.Demo2Client;
 import com.bytes.bfs.support.common.box.response.ApiResult;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.Serializable;
+
 
 @RestController
-@RequestMapping("/demo")
+@RequestMapping("/demo2")
 public class DemoController {
 
-
     @Autowired
-    private Demo2Client demo2Client;
-
-    @Data
-    @Builder
-    @AllArgsConstructor
-    @NoArgsConstructor
-    static class Request {
-        private String name;
-
-        private String id;
-    }
+    private Demo1Client demo1Client;
 
     @PostMapping("/test1")
     public ApiResult test1(){
-        return ApiResult.success("demo-test1");
+        return ApiResult.success("demo2-test1: "+ demo1Client.test1());
     }
 
     @PostMapping("/test2")
-    public ApiResult test2(){
-        ApiResult apiResult = demo2Client.test2(new JSONObject());
-        return ApiResult.success("demo-test2:  "+apiResult.getData());
+    public ApiResult<ResponseObject> test2(){
+        return ApiResult.success(ResponseObject.builder().id("12").name("hel").build());
     }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    static class ResponseObject implements Serializable{
+
+        private String id;
+
+        private String name;
+    }
+
 }
